798c919a06070ead6c923c139df136828e2ca55b
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-04 00:27:10 +0400

    feat(core): add endpoint /tasks/{task_id}

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Привет!"}
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    return {"task_id": task_id, "name": f"Задача номер {task_id}" }
