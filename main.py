317c074cd4baa4f7213a6a96118747cb6fbc910d
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-04 20:18:22 +0400

    feat(api): add GET "/tasks/{task_id}" and GET "/tasks" endpoinst (read from DB)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
-fake_db = []
+fake_db: list[dict[str, Task]] = []
 
 
 class Task(BaseModel):
@@ -12,6 +14,12 @@ class Task(BaseModel):
     title: str  # Заголовок задачи
     description: str  # Описание задачи
     is_complited: bool  # Факт выполнения задачи
+
+
+# READ Чтение всех заданий
+@app.get("/tasks")
+def get_tasks(skip: int = 0, limit: int = 10):
+    return fake_db
 
 
 #  CREATE: создаю новую задачу
@@ -31,10 +39,8 @@ def read_root():
 
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int):
-    return {"task_id": task_id, "name": f"Задача номер {task_id}"}
+    mached = [task for task in fake_db if task.get("id") == task_id]
+    return mached
 
 
-# READ Чтение всех заданий
-@app.get("/tasks")
-def get_tasks(skip: int = 0, limit: int = 10):
-    return fake_db
+# {"task:": fake_db[task_id], "name": f"Задача номер {task_id}"}
