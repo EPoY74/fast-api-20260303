6ddc3ba4ab6e75307a07b5d45c155a0bb84c2094
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-04 06:02:10 +0400

    feat(core): add endpoint /tasks with paginator

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def read_root():
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int):
     return {"task_id": task_id, "name": f"Задача номер {task_id}" }
+
+
+@app.get("/tasks")
+def get_tasks(skip: int = 0, limit: int = 10):
+    return{
+        "message": "Возвращаю список задач",
+        "skip": skip,
+        "limit": limit
+    }
