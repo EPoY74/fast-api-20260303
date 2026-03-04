1f3920a5f34252e9ccbb7b41c5a4467223bd9aa8
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-04 12:58:16 +0400

    feat(api): add GET /tasks endpoint using Pydantic schema

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def get_task(task_id: int):
     return {"task_id": task_id, "name": f"Задача номер {task_id}"}
 
 
+# READ Чтение всех заданий
 @app.get("/tasks")
 def get_tasks(skip: int = 0, limit: int = 10):
-    return {"message": "Возвращаю список задач", "skip": skip, "limit": limit}
+    return fake_db
