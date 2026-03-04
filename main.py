c5c875570054f7d263db8504e2906fb4496ebc4d
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-04 12:22:18 +0400

    feat(core): add POST /tasks endpoint using Pydantic schema

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
 
+
 class Task(BaseModel):
-    """Класс для работы с todo записью
-    """
-    title: str   # Заголовок задачи
+    """Класс для работы с todo записью"""
+
+    title: str  # Заголовок задачи
     description: str  # Описание задачи
-    is_complited: bool   # Факт выполнения задачи
+    is_complited: bool  # Факт выполнения задачи
+
+
+@app.post("/tasks")
+def creat_task(task: Task):
+    # Тут что-то делаем
+    return {"message": "задача успешно создана", "task": task}
 
 
 @app.get("/")
 def read_root():
     return {"message": "Привет!"}
 
+
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int):
-    return {"task_id": task_id, "name": f"Задача номер {task_id}" }
+    return {"task_id": task_id, "name": f"Задача номер {task_id}"}
 
 
 @app.get("/tasks")
 def get_tasks(skip: int = 0, limit: int = 10):
-    return{
-        "message": "Возвращаю список задач",
-        "skip": skip,
-        "limit": limit
-    }
+    return {"message": "Возвращаю список задач", "skip": skip, "limit": limit}
